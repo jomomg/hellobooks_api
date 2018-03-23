12df92e926dab17f03779c843c8cb7f02e91702b
Jomo <jomomg@gmail.com>
2018-03-23 21:36:07 +0300

    Add config options for the app

---
+++ config.py
@@ -0,0 +1,23 @@
+class Config:
+    """Parent config class"""
+    DEBUG = False
+    SECRET = '!!@#YDGGJGJGKJasdfadsff12526263JGKJH&*&^**IGHBJHB'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    """Configurations for development"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
