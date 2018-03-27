40803946135bab2e0808241fc38b2bb67bc785e6
Jomo <jomomg@gmail.com>
2018-03-27 11:32:40 +0300

    Add User and BorrowLog models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
-users_list = []  # list containing all the users
-books_list = []  # list containing all the books
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+
+users_list = []      # list containing all the users
+books_list = []      # list containing all the books
 borrowing_info = []  # list containing all borrowing information
+blacklist = set()    # token blacklist
 
 
 class Book:
@@ -16,9 +20,6 @@ class Book:
         self.subcategory = None
         self.description = None
         self.available = 0
-
-    def borrow(self):
-        pass
 
     def save(self):
         books_list.append(self)
@@ -36,3 +37,57 @@ class Book:
     @staticmethod
     def get_all():
         return books_list
+
+
+class User:
+    """class containing all the user information"""
+
+    def __init__(self):
+        self.id = None
+        self.first_name = None
+        self.last_name = None
+        self.email = None
+        self.password = None
+        self.is_admin = False
+        self.books_borrowed = []
+
+    def password(self):
+        raise AttributeError
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def save(self):
+        users_list.append(self)
+
+    def borrow_book(self, book_id):
+        book = Book.get_by_id(book_id)
+        self.books_borrowed.append(book)
+        now = datetime.datetime.now()
+        record = BorrowLog(id=book.id,
+                           user=self,
+                           book=book,
+                           borrow_date=now.strftime("%d/%m/%Y"))
+        record.save()
+
+    @staticmethod
+    def get_by_email(email):
+        for user in users_list:
+            if user.email == email:
+                return user
+
+
+class BorrowLog:
+    """class containing log of all books borrowed"""
+
+    def __init__(self, id, user, book, borrow_date):
+        self.id = id
+        self.user = user  # user who has borrowed the book
+        self.book = book  # book that has been borrowed
+        self.borrow_date = borrow_date
+
+    def save(self):
+        borrowing_info.append(self)
