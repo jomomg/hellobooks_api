cab6dd8e6b30870999fd29c6005d044502994029
Jomo <jomomg@gmail.com>
2018-04-01 22:29:37 +0300

    Add database configurations

--- app/models.py
+++ app/models.py
@@ -5,90 +5,6 @@ import datetime
 from app import db
 
 
-<<<<<<< HEAD
-users_list = []
-books_list = []
-borrowing_info = []
-# token blacklist
-blacklist = set()
-
-
-class Book:
-    """Class containing all the book information"""
-
-    book_id = 0
-
-    def __init__(self, title='', publisher='', 
-        category='', subcategory='', description='', 
-        pub_year=''):
-
-        self.id = id
-        self.title = title
-        self.publisher = publisher
-        self.publication_year = pub_year
-        self.edition = 1
-        self.category = category
-        self.subcategory = subcategory
-        self.description = description
-        self.is_borrowed = False
-
-    def generate_id(self):
-        Book.book_id += 1
-        self.id = Book.book_id
-
-    def save(self):
-        """Save current instance in the books list"""
-
-        self.generate_id()
-        books_list.append(self)
-
-    def delete(self):
-        """Delete the current instance in the books list"""
-
-        books_list.remove(self)
-
-    @staticmethod
-    def get_by_id(book_id):
-        """Get a book by its id"""
-
-        for book in books_list:
-            if book.id == book_id:
-                return book
-        return None
-
-    @staticmethod
-    def get_all():
-        """Get all books in the books list"""
-
-        return books_list
-
-    def serialize(self):
-        """Return a dictionary object with all the book details"""
-
-        return {
-            'book_id': self.id,
-            'book_title': self.title,
-            'publisher': self.publisher,
-            'publication_year': self.publication_year,
-            'edition': self.edition,
-            'category': self.category,
-            'subcategory': self.subcategory,
-            'description': self.description
-        }
-
-
-class User:
-    """Class containing all the user information"""
-
-    def __init__(self):
-        self.id = None
-        self.email = None
-        self.password = None
-        self.is_admin = False
-        self.books = []
-        self.books_borrowed = []
-
-=======
 blacklist = set()    # token blacklist
 
 
@@ -138,7 +54,6 @@ class User(db.Model):
     is_admin = db.Column(db.String)
     borrowed_books = db.relationship('BorrowLog', backref='user')
 
->>>>>>> Create Book, User and BorrowLog models
     def set_password(self, password):
         """Generate a password hash"""
 
@@ -150,24 +65,13 @@ class User(db.Model):
         return check_password_hash(self.password, password)
 
     def save(self):
-<<<<<<< HEAD
-        """Save current instance in the users list"""
-
-        users_list.append(self)
-=======
         db.session.add(self)
         db.session.commit()
->>>>>>> Create Book, User and BorrowLog models
 
     def borrow_book(self, book_id):
         """Borrow a book"""
 
         book = Book.get_by_id(book_id)
-<<<<<<< HEAD
-        self.books_borrowed.append(book)
-        book.is_borrowed = True
-=======
->>>>>>> Create Book, User and BorrowLog models
         now = datetime.datetime.now()
         record = BorrowLog(user_id=self.id,
                            book_id=book.id,
@@ -182,28 +86,6 @@ class User(db.Model):
 
     @staticmethod
     def get_by_email(email):
-<<<<<<< HEAD
-        """Retrieve a user by their email"""
-
-        for user in users_list:
-            if user.email == email:
-                return user
-
-
-class BorrowLog:
-    """Class containing log of all books borrowed"""
-
-    def __init__(self, id, user, book, borrow_date):
-        self.id = id
-        self.user = user
-        self.book = book
-        self.borrow_date = borrow_date
-
-    def save(self):
-        """Save borrowing record"""
-
-        borrowing_info.append(self)
-=======
         return User.query.filter_by(email=email).first()
 
     def __repr__(self):
@@ -224,4 +106,3 @@ class BorrowLog(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
->>>>>>> Create Book, User and BorrowLog models
--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class Config:
     """Parent config class"""
 
     DEBUG = False
+    CSRF_ENABLED = True
     SECRET_KEY = '!!@#YDGGJGJGKJasdfadsff12526263JGKJH&*&^**IGHBJHB'
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///hellobooks_api'
 
     @staticmethod
     def init_app(app):
@@ -28,6 +30,7 @@ class TestingConfig(Config):
 
     TESTING = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///test_db'
 
 app_config = {
     'development': DevelopmentConfig,
