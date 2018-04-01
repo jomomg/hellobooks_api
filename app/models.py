6ad083c5df7d74cc15f5ab8e78e4949188484c49
Jomo <jomomg@gmail.com>
2018-04-01 22:27:20 +0300

    Create Book, User and BorrowLog models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,10 @@
 
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
+from app import db
 
+
+<<<<<<< HEAD
 users_list = []
 books_list = []
 borrowing_info = []
@@ -85,6 +88,57 @@ class User:
         self.books = []
         self.books_borrowed = []
 
+=======
+blacklist = set()    # token blacklist
+
+
+class Book(db.Model):
+    """class containing all the book information"""
+
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    publisher = db.Column(db.String)
+    publication_year = db.Column(db.String)
+    edition = db.Column(db.String)
+    category = db.Column(db.String)
+    subcategory = db.Column(db.String)
+    description = db.Column(db.String)
+    available = db.Column(db.Integer, default=1)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_by_id(book_id):
+        return Book.query.get(book_id)
+
+    @staticmethod
+    def get_all():
+        return Book.query.all()
+
+    def __repr__(self):
+        return '<Book: {}>'.format(self.title)
+
+
+class User(db.Model):
+    """class containing all the user information"""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    is_admin = db.Column(db.String)
+    borrowed_books = db.relationship('BorrowLog', backref='user')
+
+>>>>>>> Create Book, User and BorrowLog models
     def set_password(self, password):
         """Generate a password hash"""
 
@@ -96,25 +150,39 @@ class User:
         return check_password_hash(self.password, password)
 
     def save(self):
+<<<<<<< HEAD
         """Save current instance in the users list"""
 
         users_list.append(self)
+=======
+        db.session.add(self)
+        db.session.commit()
+>>>>>>> Create Book, User and BorrowLog models
 
     def borrow_book(self, book_id):
         """Borrow a book"""
 
         book = Book.get_by_id(book_id)
+<<<<<<< HEAD
         self.books_borrowed.append(book)
         book.is_borrowed = True
+=======
+>>>>>>> Create Book, User and BorrowLog models
         now = datetime.datetime.now()
-        record = BorrowLog(id=book.id,
-                           user=self,
-                           book=book,
-                           borrow_date=now.strftime("%d/%m/%Y"))
+        record = BorrowLog(user_id=self.id,
+                           book_id=book.id,
+                           borrow_timestamp=now)
         record.save()
+
+    def return_book(self):
+        pass
+
+    def borrowing_history(self):
+        pass
 
     @staticmethod
     def get_by_email(email):
+<<<<<<< HEAD
         """Retrieve a user by their email"""
 
         for user in users_list:
@@ -135,3 +203,25 @@ class BorrowLog:
         """Save borrowing record"""
 
         borrowing_info.append(self)
+=======
+        return User.query.filter_by(email=email).first()
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.email)
+
+
+class BorrowLog(db.Model):
+    """class containing log of all books borrowed"""
+
+    __tablename__ = 'borrow_log'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)  # user who has borrowed the book
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), primary_key=True)  # book that has been borrowed
+    borrow_timestamp = db.Column(db.DateTime)
+    returned = db.Column(db.Boolean)
+    return_timestamp = db.Column(db.DateTime)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+>>>>>>> Create Book, User and BorrowLog models
