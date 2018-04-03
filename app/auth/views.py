0a0ccd30296f816f8a83a3b344a2162865fb66f2
Jomo <jomomg@gmail.com>
2018-04-03 23:07:33 +0300

    Add validation checks for email and password

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,21 +1,28 @@
-from . import auth
-
+import re
 from flask import request, jsonify
-from app.models import User, blacklist
 from flask_jwt_extended import (create_access_token,
                                 get_jwt_identity,
                                 jwt_required,
                                 get_raw_jwt)
 
+from app.models import User, blacklist
+from . import auth
+
 
 @auth.route('/api/v1/auth/register', methods=['POST'])
 def register_user():
-    """Registers a new user"""
+    """Register a new user"""
 
     if request.method == 'POST':
         email = request.data['email']
         password = request.data['password']
         user = User.get_by_email(email)
+
+        if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$', email):
+            return jsonify({'message': 'Please enter a valid email address'}), 202
+
+        if not re.match(r'^[a-zA-Z0-9*&#!@^._%+-]', password):
+            return jsonify({'message': 'Please enter a valid password'})
 
         if not user:
             new_user = User()
@@ -23,19 +30,19 @@ def register_user():
             new_user.set_password(password)
             new_user.save()
 
-            response = jsonify({'message': 'successful registration'})
+            response = jsonify({'message': 'Successful registration'})
             response.status_code = 201
             return response
 
         else:
-            response = jsonify({'message': 'you are already registered'})
+            response = jsonify({'message': 'This account has already been registered'})
             response.status_code = 202
             return response
 
 
 @auth.route('/api/v1/auth/login', methods=['POST'])
 def login_user():
-    """Logs a user in"""
+    """Log a user in"""
 
     if request.method == 'POST':
         email = request.data['email']
@@ -44,11 +51,11 @@ def login_user():
 
         if user and user.check_password(password):
             access_token = create_access_token(identity=email)
-            response = {'message': 'successful login', 'access_token': access_token}
+            response = {'message': 'Successful login', 'access_token': access_token}
             return jsonify(response), 200
 
         else:
-            response = jsonify({'message': 'invalid email or password'})
+            response = jsonify({'message': 'Invalid email or password'})
             response.status_code = 401
             return response
 
@@ -56,14 +63,14 @@ def login_user():
 @auth.route('/api/v1/auth/reset-password', methods=['POST'])
 @jwt_required
 def reset_password():
-    """resets a user's password"""
+    """Reset a user's password"""
 
     email = get_jwt_identity()
     current_user = User().get_by_email(email)
     new_password = request.data['password']
 
     current_user.set_password(new_password)
-    response = jsonify({'message': 'password reset successful'})
+    response = jsonify({'message': 'Password reset successful'})
     response.status_code = 200
     return response
 
@@ -71,10 +78,10 @@ def reset_password():
 @auth.route('/api/v1/auth/logout', methods=['POST'])
 @jwt_required
 def logout():
-    """logs a user out by revoking access token"""
+    """Log a user out by revoking access token"""
 
     jti = get_raw_jwt()['jti']
     blacklist.add(jti)
-    response = jsonify({'message': 'successfully logged out'})
+    response = jsonify({'message': 'Successfully logged out'})
     response.status_code = 200
     return response
