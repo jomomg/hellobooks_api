3aa4a503442c5787e8c5e2a5c71e1e8ab665d5ea
Jomo <jomomg@gmail.com>
2018-05-21 15:42:51 +0300

    Add mail config options

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """Application configuration options"""
 
 import datetime
+import os
 
 
 class Config:
@@ -12,9 +13,21 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access']
+    JWT_TOKEN_LOCATION = ['headers', 'query_string']
+    JWT_QUERY_STRING_NAME = 'token'
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/hellobooks_api'
     ADMIN = ['jomo@user.com']
     BOOK_RETURN_PERIOD = 14  # days
+    DOMAIN = 'http://127.0.0.1:5000'
+
+    # mail configuration
+    MAIL_DEFAULT_SENDER = 'jomomg@gmail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('USERNAME')
+    MAIL_PASSWORD = os.environ.get('PASS')
 
     @staticmethod
     def init_app(app):
